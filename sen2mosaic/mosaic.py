8af591249ab932a099408b5566d70f81509e5374
sambowers <sam.bowers@ed.ac.uk>
2019-01-25 17:55:14 +0000

    Added additional masking options, and now outputing SLC mask

--- sen2mosaic/mosaic.py
+++ sen2mosaic/mosaic.py
@@ -106,20 +106,23 @@ def _getImageOrder(scenes, image_n):
     return tile_number
 
 
-def _nan_percentile(arr, q):
+def _nan_percentile(arr, quant):
     """
     Function to calculate a percentile along the first axis of a 3d array, much faster than np.nanpercentile.
-    From: https://krstn.eu/np.nanpercentile()-there-has-to-be-a-faster-way/
+    Modified with permission from: https://krstn.eu/np.nanpercentile()-there-has-to-be-a-faster-way/
     
     Args:
         arr: Three dimensional numpy array (first dimension to be reduced)
-        q: Percentile, in percent
+        quant: Percentile, in percent
     Returns:
-        A two dimensional numpy array containing data from percentile q
+        A two dimensional numpy array containing data from percentile quant
     """
-
+    
+    assert quant >= 0 and quant <= 100, "Quantile must be between 0 % and 100 %."
+    
     def _zvalue_from_index(arr, ind):
-        """private helper function to work around the limitation of np.choose() by employing np.take()
+        """
+        Private helper function to work around the limitation of np.choose() by employing np.take()
         arr has to be a 3D array
         ind has to be a 2D array containing values for z-indicies to take from arr
         See: http://stackoverflow.com/a/32091712/4169585
@@ -128,52 +131,44 @@ def _nan_percentile(arr, q):
         # get number of columns and rows
         _,nC,nR = arr.shape
         
-        # get linear indices and extract elements with np.take()
+        # Get linear indices and extract elements with np.take()
         #idx = nC*nR*ind + nC*np.arange(min(nC,nR))[:,None] + np.arange(max(nC,nR))
         idx = nC*nR*ind + np.arange(nC*nR).reshape((nC,nR))
-        return np.take(arr, idx)
         
-    # valid (non NaN) observations along the first axis
+        return np.take(arr, idx)
+    
+    # Valid (non NaN) observations along the first axis
     valid_obs = np.sum(np.isfinite(arr), axis=0)
-    # replace NaN with maximum
+    
+    # Replace NaN with maximum
     max_val = np.nanmax(arr)
     arr[np.isnan(arr)] = max_val
-    # sort - former NaNs will move to the end
-    arr = np.sort(arr, axis=0)
-
-    # loop over requested quantiles
-    if type(q) is list:
-        qs = []
-        qs.extend(q)
-    else:
-        qs = [q]
-    if len(qs) < 2:
-        quant_arr = np.zeros(shape=(arr.shape[1], arr.shape[2]))
-    else:
-        quant_arr = np.zeros(shape=(len(qs), arr.shape[1], arr.shape[2]))
     
-    result = []
-    for i in range(len(qs)):
-        quant = qs[i]
-        # desired position as well as floor and ceiling of it
-        k_arr = (valid_obs - 1) * (quant / 100.0)
-        f_arr = np.floor(k_arr).astype(np.int32)
-        c_arr = np.ceil(k_arr).astype(np.int32)
-        fc_equal_k_mask = f_arr == c_arr
+    # Sort - former NaNs will move to the end
+    arr = np.sort(arr, axis=0)
+    
+    # Build output array
+    quant_arr = np.zeros(shape=(arr.shape[1], arr.shape[2]))
 
-        # linear interpolation (like numpy percentile) takes the fractional part of desired position
-        floor_val = _zvalue_from_index(arr=arr, ind=f_arr) * (c_arr - k_arr)
-        ceil_val = _zvalue_from_index(arr=arr, ind=c_arr) * (k_arr - f_arr)
+    # Desired position as well as floor and ceiling of it
+    k_arr = (valid_obs - 1) * (quant / 100.0)
+    f_arr = np.floor(k_arr).astype(np.int32)
+    c_arr = np.ceil(k_arr).astype(np.int32)
+    
+    # Floor == Ceil
+    fc_equal_k_mask = f_arr == c_arr
 
-        quant_arr = floor_val + ceil_val
-        quant_arr[fc_equal_k_mask] = _zvalue_from_index(arr=arr, ind=k_arr.astype(np.int32))[fc_equal_k_mask]  # if floor == ceiling take floor value
+    # Linear interpolation (like numpy percentile) takes the fractional part of desired position
+    floor_val = _zvalue_from_index(arr = arr, ind = f_arr) * (c_arr - k_arr)
+    ceil_val = _zvalue_from_index(arr = arr, ind = c_arr) * (k_arr - f_arr)
+    
+    quant_arr = floor_val + ceil_val
+    quant_arr[fc_equal_k_mask] = _zvalue_from_index(arr = arr, ind = k_arr.astype(np.int32))[fc_equal_k_mask]  # if floor == ceiling take floor value
 
-        result.append(quant_arr)
-
-    return result
+    return np.round(quant_arr,0).astype(np.uint16)
 
 
-def _makeBlocks(band, scene, step = 2000, percentile = 25., cloud_buffer = 180):
+def _makeBlocks(band, scene, step = 2000, percentile = 25., cloud_buffer = 0, masked_vals = []):
     '''
     Function to build a series of blocks of size (step, step) to enable multiprocessing and prevent overloading memory
     
@@ -195,7 +190,7 @@ def _makeBlocks(band, scene, step = 2000, percentile = 25., cloud_buffer = 180):
         for row in np.arange(0, scene.metadata.nrows, step):
             row_step = step if row + step <= scene.metadata.nrows else scene.metadata.nrows - row
             
-            blocks.append([band, col, col_step, row, row_step, percentile, cloud_buffer])
+            blocks.append([band, col, col_step, row, row_step, percentile, cloud_buffer, masked_vals])
     
     return blocks
 
@@ -205,12 +200,12 @@ def _doComposite(input_list):
     Internal function for buildMosaic.
     
     Args:
-        input_list: A list containing band, col, col_step, row, row_step from _makeBlocks(), and percentile, cloud_buffer
+        input_list: A list containing band, col, col_step, row, row_step from _makeBlocks(), percentile, cloud_buffer, and masked_vals 
     Returns:
         A composite image for input block
     '''
     
-    band, col, col_step, row, row_step, percentile, cloud_buffer = input_list
+    band, col, col_step, row, row_step, percentile, cloud_buffer, masked_vals = input_list
 
     # Mask stack
     m = np.zeros((len(scenes_tile), col_step, row_step), dtype = np.uint8)
@@ -224,41 +219,63 @@ def _doComposite(input_list):
         
         b[n,:,:] = scene.getBand(band, chunk = (row,col,row_step,col_step))
     
-    # If nodata in the entire chuck, skip processing
+    # If nodata in the entire chunk, skip processing
     if m.sum() == 0: return b
     
     bm = np.ma.array(b, mask = np.ones_like(m,dtype=np.bool))
     
     # Add pixels in order of desirability
     nodata = np.ones_like(b[0,:,:], dtype = np.bool)
-    quality = np.zeros_like(b[0,:,:], dtype = np.uint8)
-    
-    for n, vals in enumerate([[4,5,6],[2,7,11],[1,3,8,10],[9]]):#[[4,5,6],[1,2],[7,11],[12],[3],[10],[8],[9]]):
+    slc = np.zeros_like(b[0,:,:], dtype = np.uint8)
+    slc_count = np.zeros_like(b[0,:,:], dtype = np.uint8)
+    slc_assigned = np.zeros_like(b[0,:,:], dtype = np.bool)
+
+    for n, vals in enumerate([[4,5,6], [2,7,11], [1,3,8,10], [9]]):
+        
+        # Strip masked values from vals
+        for masked_val in masked_vals:
+            if masked_val in vals:
+                vals.remove(masked_val)
+        
+        # Skip if all vals are in masked_vals
+        if len(vals) == 0: continue
         
         bm[:,nodata] = np.ma.array(b, mask = np.isin(m, vals) == False)[:,nodata]
         
-        quality[nodata] = n + 1
+        # Calculate modal SLC value
+        for val in vals:
+            this_count = (m == val).sum(axis = 0)
+            slc[np.logical_and(this_count > slc_count, slc_assigned == False)] = val
+            slc_count[this_count > slc_count] = this_count[this_count > slc_count]
         
-        nodata = (bm.mask == False).sum(axis=0) == 0
-       
+        slc_assigned[slc_count != 0] = True
+        
+        nodata = (bm.mask == False).sum(axis = 0) == 0
+            
     bm = np.ma.filled(bm, np.nan)
     
-    bm = _nan_percentile(bm, percentile)[0]
+    bm = _nan_percentile(bm, percentile)
     
-    return bm, quality
+    # Set nodata to 0
+    bm[nodata] = 0
+                
+    return bm, slc
        
 
-def buildMosaic(scenes, band, md_dest, output_dir = os.getcwd(), output_name = 'mosaic', step = 2000, cloud_buffer = 180, processes = 1, percentile = 25., colour_balance = False, verbose = False):
+def buildMosaic(scenes, band, md_dest, output_dir = os.getcwd(), output_name = 'mosaic', step = 2000, cloud_buffer = 0, processes = 1, percentile = 25., colour_balance = False, masked_vals = [0,1,2,3,7,8,9,10,11], verbose = False):
     """
     """
-    
+        
     global scenes_tile
     
-    # Sort scenes to minimise artefacts
+    for m in masked_vals:
+        assert type(m) == int, "Masked values must all be integers."
+    
+    # Sort scenes for tidiness
     scenes_sorted = utilities.sortScenes(scenes)
         
     composite_out = md_dest.createBlankArray(dtype = np.uint16)
-    quality_out = md_dest.createBlankArray(dtype = np.uint8)
+    slc_out = md_dest.createBlankArray(dtype = np.uint8)
     
     # Process one Sentinel-2 tile at a time
     for tile in np.unique([s.tile for s in scenes_sorted]):
@@ -268,9 +285,9 @@ def buildMosaic(scenes, band, md_dest, output_dir = os.getcwd(), output_name = '
         scene = scenes_tile[0]
                 
         composite = np.zeros((scene.metadata.ncols, scene.metadata.nrows), dtype = np.uint16)
-        quality = np.zeros((scene.metadata.ncols, scene.metadata.nrows), dtype = np.uint8)
+        slc = np.zeros((scene.metadata.ncols, scene.metadata.nrows), dtype = np.uint8)
         
-        blocks = _makeBlocks(band, scene, step = step, percentile = percentile, cloud_buffer = cloud_buffer)
+        blocks = _makeBlocks(band, scene, step = step, percentile = percentile, cloud_buffer = cloud_buffer, masked_vals = masked_vals)
         
         if processes == 1:
             composite_parts = [_doComposite(block) for block in blocks]
@@ -281,13 +298,13 @@ def buildMosaic(scenes, band, md_dest, output_dir = os.getcwd(), output_name = '
         
         # Reconsitute image
         for n, block in enumerate(blocks):
-            band, col, col_step, row, row_step, _, _ = block
+            band, col, col_step, row, row_step, _, _, _ = block
             composite[col:col+col_step,row:row+row_step] = composite_parts[n][0]
-            quality[col:col+col_step,row:row+row_step] = composite_parts[n][1]
+            slc[col:col+col_step,row:row+row_step] = composite_parts[n][1]
         
         # Reproject to match output array
         composite_rep = utilities.reprojectBand(scene, composite, md_dest, dtype = 3, resampling = 0)
-        quality_rep = utilities.reprojectBand(scene, quality, md_dest, dtype = 1, resampling = 0)
+        slc_rep = utilities.reprojectBand(scene, slc, md_dest, dtype = 1, resampling = 0)
         
         # Do optional colour balancing
         if colour_balance:
@@ -297,13 +314,13 @@ def buildMosaic(scenes, band, md_dest, output_dir = os.getcwd(), output_name = '
         # Add pixels to the output mosaic
         sel = composite_rep!=0
         composite_out[sel] = composite_rep[sel]
-        quality_out[sel] = quality_rep[sel]
+        slc_out[sel] = slc_rep[sel]
         
-    utilities.createGdalDataset(md_dest, data_out = composite_out, filename = '%s/%s_R%sm_%s.tif'%(output_dir, output_name, str(scene.resolution), band), driver='GTiff', options = ['COMPRESS=LZW'])        
+    utilities.createGdalDataset(md_dest, data_out = composite_out, filename = '%s/%s_R%sm_%s.tif'%(output_dir, output_name, str(scene.resolution), band), driver='GTiff', nodata = 0, options = ['COMPRESS=LZW'])        
     
-    utilities.createGdalDataset(md_dest, data_out = quality_out, filename = '%s/%s_R%sm_%s_QA.tif'%(output_dir, output_name, str(scene.resolution), band), driver='GTiff', options = ['COMPRESS=LZW'])        
+    utilities.createGdalDataset(md_dest, data_out = slc_out, filename = '%s/%s_R%sm_%s_SLC.tif'%(output_dir, output_name, str(scene.resolution), band), driver='GTiff', options = ['COMPRESS=LZW'])        
     
-    return composite_out, quality_out
+    return composite_out, slc_out
         
 
 def buildVRT(red_band, green_band, blue_band, output_path):
@@ -330,7 +347,7 @@ def buildVRT(red_band, green_band, blue_band, output_path):
     subprocess.call(command)
 
 
-def main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetime.datetime.today().strftime('%Y%m%d'), resolution = 0, correct_mask = True, cloud_buffer = 0., colour_balance = False, processes = 1, output_dir = os.getcwd(), output_name = 'mosaic', verbose = False):
+def main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetime.datetime.today().strftime('%Y%m%d'), resolution = 0, correct_mask = True, cloud_buffer = 0., colour_balance = False, processes = 1, output_dir = os.getcwd(), output_name = 'mosaic', masked_vals = 'auto', verbose = False):
     """main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetime.datetime.today().strftime('%Y%m%d'), resolution = 0, correct_mask = True, cloud_buffer = 0., colour_balance = False, processes = 1, output_dir = os.getcwd(), output_name = 'mosaic', verbose = False):
     
     Function to generate seamless mosaics from a list of Sentinel-2 level-2A input files.
@@ -346,6 +363,7 @@ def main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetim
         processes: Number of processes to run similtaneously. Defaults to 1.
         output_dir: Optionally specify an output directory.
         output_name: Optionally specify a string to precede output file names. Defaults to 'mosaic'.
+        masked_vals: List of SLC mask values to not include in the final mosaic. Defaults to 'auto', which masks everything except [4,5,6]
         verbose: Make script verbose (set True).
     """
     
@@ -358,6 +376,10 @@ def main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetim
     output_dir = os.path.abspath(os.path.expanduser(output_dir))
     assert os.path.exists(output_dir), "Output directory (%s) does not exist."%output_dir
     assert os.access(output_dir, os.W_OK), "Output directory (%s) does not have write permission. Try setting a different output directory, or changing permissions with chmod."%output_dir
+    
+    if masked_vals == 'auto': masked_vals = [0,1,2,3,7,8,9,10,11]
+    if masked_vals == 'none': masked_vals = []
+    assert type(masked_vals) == list, "Masked values must be a list of integers, or set to 'auto' or 'none'."
     
     res_list, band_list = _getBands(resolution)
     
@@ -389,7 +411,7 @@ def main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetim
             
             if verbose: print 'Building band %s at %s m resolution'%(band, str(res))
             
-            band_out, QA_out = buildMosaic(scenes_reduced, band, md_dest, output_dir = output_dir, output_name = output_name, colour_balance = colour_balance, cloud_buffer = cloud_buffer, percentile = 25., processes = processes, step = 2000, verbose = verbose)            
+            band_out, QA_out = buildMosaic(scenes_reduced, band, md_dest, output_dir = output_dir, output_name = output_name, colour_balance = colour_balance, cloud_buffer = cloud_buffer, percentile = 25., processes = processes, step = 2000, masked_vals = masked_vals, verbose = verbose)            
            
         # Build VRT output files for straightforward visualisation
         if verbose: print 'Building .VRT images for visualisation'
@@ -426,8 +448,9 @@ if __name__ == "__main__":
     optional.add_argument('-st', '--start', type = str, default = '20150101', help = "Start date for tiles to include in format YYYYMMDD. Defaults to processing all dates.")
     optional.add_argument('-en', '--end', type = str, default = datetime.datetime.today().strftime('%Y%m%d'), help = "End date for tiles to include in format YYYYMMDD. Defaults to processing all dates.")
     optional.add_argument('-res', '--resolution', metavar = '10/20/60', type=int, default = 0, help="Specify a resolution to process (10, 20, 60, or 0 for all).")
-    optional.add_argument('-b', '--colour_balance', action='store_true', default = False, help = "Perform colour balancing between scenes. Defaults to False.")
-    optional.add_argument('-c', '--cloud_buffer', type=int, metavar = 'M', default = 0, help = "Apply improvements to sen2cor cloud mask by applying a buffer. Defaults to no buffer.")
+    optional.add_argument('-m', '--masked_vals', metavar = 'N', type=str, nargs='*', default = ['auto'], help="Specify SLC values to not include in the mosaic (e.g. -m 7 8 9). See http://step.esa.int/main/third-party-plugins-2/sen2cor/ for description of sen2cor mask values. Defaults to 'auto', which masks values [0, 1, 2, 3, 7, 8, 9, 10, 11]. Also accepts 'none'.")
+    optional.add_argument('-b', '--colour_balance', action='store_true', default = False, help = "Perform colour balancing between tiles. Defaults to False.")
+    optional.add_argument('-c', '--cloud_buffer', type=int, metavar = 'M', default = 0, help = "Apply improvements to sen2cor cloud mask by applying a buffer around cloudy pixels (in meters). Not generally recommended, except in cloudy areas or where a very conservative mask is desired. Defaults to no buffer.")
     optional.add_argument('-p', '--n_processes', type = int, metavar = 'N', default = 1, help = "Specify a maximum number of tiles to process in paralell. Bear in mind that more processes will require more memory. Defaults to 1.")
     optional.add_argument('-o', '--output_dir', type=str, metavar = 'DIR', default = os.getcwd(), help="Specify an output directory. Defaults to the present working directory.")
     optional.add_argument('-n', '--output_name', type=str, metavar = 'NAME', default = 'mosaic', help="Specify a string to precede output filename. Defaults to 'mosaic'.")
@@ -438,12 +461,18 @@ if __name__ == "__main__":
     
     assert args.level in ['1C', '2A'], "Input level much be '1C' or '2A'."
     
+    # Convert masked_vals to integers, where specified
+    if args.masked_vals != ['auto'] and args.masked_vals != ['none']:
+        masked_vals = [int(m) for m in args.masked_vals]
+    else:
+        masked_vals = args.masked_vals[0]
+    
     # Get absolute path of input .safe files.
     infiles = [os.path.abspath(i) for i in args.infiles]
     
     # Find all matching granule files
     infiles = utilities.prepInfiles(infiles, args.level)
     
-    main(infiles, args.target_extent, args.epsg, resolution = args.resolution, start = args.start, end = args.end, cloud_buffer = args.cloud_buffer, colour_balance = args.colour_balance, processes = args.n_processes, output_dir = args.output_dir, output_name = args.output_name, verbose = args.verbose)
+    main(infiles, args.target_extent, args.epsg, resolution = args.resolution, start = args.start, end = args.end, cloud_buffer = args.cloud_buffer, colour_balance = args.colour_balance, processes = args.n_processes, output_dir = args.output_dir, output_name = args.output_name, masked_vals = masked_vals, verbose = args.verbose)
     
     
