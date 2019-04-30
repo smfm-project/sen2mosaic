78a04195540448fbeabff9167e94da8345eff170
sambowers <sam.bowers@ed.ac.uk>
2019-04-30 18:30:42 +0100

    Reformatted to allow easier use of Python interface

---
+++ cli/mosaic.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import numpy as np
+import os
+
+import sen2mosaic.core
+import sen2mosaic.mosaic
+import sen2mosaic.utilities
+
+import pdb
+
+
+def _getBands(resolution):
+    '''
+    Get a list of Sentinel-2 bands given an input resolution
+    
+    Args:
+        resolution: Sentinel-2 resolution (10, 20, or 60 m)
+    Returns:
+        A list of resolutions
+        A list of band names
+    '''
+    
+    band_list = []
+    res_list = []
+    
+    if resolution == 60 or resolution == 0:
+        band_list.extend(['B01','B02','B03','B04','B05','B06','B07','B8A','B09','B11','B12'])
+        res_list.extend([60] * 11)
+        
+    if resolution == 20 or resolution == 0:
+        band_list.extend(['B02','B03','B04','B05','B06','B07','B8A','B11','B12'])
+        res_list.extend([20] * 9)
+        
+    if resolution == 10 or resolution == 0:
+        band_list.extend(['B02','B03','B04','B08'])
+        res_list.extend([10] * 4)
+        
+    return np.array(res_list), np.array(band_list)
+
+
+
+def main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetime.datetime.today().strftime('%Y%m%d'), resolution = 0, improve_mask = False, colour_balance = False, processes = 1, output_dir = os.getcwd(), output_name = 'mosaic', masked_vals = 'auto', temp_dir = '/tmp', verbose = False):
+    """main(source_files, extent_dest, EPSG_dest, start = '20150101', end = datetime.datetime.today().strftime('%Y%m%d'), resolution = 0, improve_mask = False, colour_balance = False, processes = 1, output_dir = os.getcwd(), output_name = 'mosaic', masked_vals = 'auto', temp_dir = '/tmp', verbose = False)
+    
+    Function to generate seamless mosaics from a list of Sentinel-2 level-2A input files.
+        
+    Args:
+        source_files: A list of level 3A input files.
+        extent_dest: List desciribing corner coordinate points in destination CRS [xmin, ymin, xmax, ymax].
+        EPSG_dest: EPSG code of destination coordinate reference system. Must be a UTM projection. See: https://www.epsg-registry.org/ for codes.
+        start: Start date to process, in format 'YYYYMMDD' Defaults to start of Sentinel-2 era.
+        end: End date to process, in format 'YYYYMMDD' Defaults to today's date.
+        resolution: Process 10, 20, or 60 m bands. Defaults to processing all three.
+        improve_mask: Set True to apply improvements Sentinel-2 cloud mask. Not generally recommended.
+        processes: Number of processes to run similtaneously. Defaults to 1.
+        output_dir: Optionally specify an output directory.
+        output_name: Optionally specify a string to precede output file names. Defaults to 'mosaic'.
+        masked_vals: List of SLC mask values to not include in the final mosaic. Defaults to 'auto', which masks everything except [4,5,6]
+        temp_dir: Directory to temporarily write L1C mask files. Defaults to /tmp
+        verbose: Make script verbose (set True).
+    """
+    
+    # Test input formatting
+    assert len(extent_dest) == 4, "Output extent must be specified in the format [xmin, ymin, xmax, ymax]"
+    assert extent_dest[0] < extent_dest[2], "Output extent incorrectly specified: xmin must be lower than xmax."
+    assert extent_dest[1] < extent_dest[3], "Output extent incorrectly specified: ymin must be lower than ymax."
+    assert len(source_files) >= 1, "No source files in specified location."
+    assert resolution in [0, 10, 20, 60], "Resolution must be 10, 20, or 60 m, or 0 to process all three."
+    assert type(improve_mask) == bool, "improve_mask can only be set to True or False."
+    
+    # Test that output directory is writeable
+    output_dir = os.path.abspath(os.path.expanduser(output_dir))
+    assert os.path.exists(output_dir), "Output directory (%s) does not exist."%output_dir
+    assert os.access(output_dir, os.W_OK), "Output directory (%s) does not have write permission. Try setting a different output directory, or changing permissions with chmod."%output_dir
+    
+    # Set values to be masked
+    if masked_vals == 'auto': masked_vals = [0,9]#[0,1,2,3,7,8,9,10,11]
+    if masked_vals == 'none': masked_vals = []
+    assert type(masked_vals) == list, "Masked values must be a list of integers, or set to 'auto' or 'none'."
+    
+    res_list, band_list = _getBands(resolution)
+    
+    # For each of the input resolutions
+    for res in np.unique(res_list)[::-1]:
+        
+        # Load metadata for all Sentinel-2 datasets
+        scenes = []
+        for source_file in source_files:
+            try:
+                scenes.append(sen2mosaic.core.LoadScene(source_file, resolution = res))
+            except Exception as e:
+                print(e)
+                print('WARNING: Error in loading scene %s. Continuing.'%source_file)
+        
+        assert len(scenes) > 0, "Failed to load any scenes for resolution %sm. Check input scenes."%str(res)
+        
+        # Build metadata of output object
+        md_dest = sen2mosaic.core.Metadata(extent_dest, res, EPSG_dest)
+        
+        # Reduce the pool of scenes to only those that overlap with output tile
+        scenes_reduced = sen2mosaic.utilities.getSourceFilesInTile(scenes, md_dest, start = start, end = end, verbose = verbose)       
+        
+        # It's only worth processing a tile if at least one input image is inside tile
+        if len(scenes_reduced) == 0:
+            print("    No data inside specified output area for resolution %s. make sure you specified your bouding box in the correct order (i.e. xmin ymin xmax ymax) and EPSG code correctly. Continuing."%str(res))
+            continue        
+        
+        output_mask = True
+        for band in band_list[res_list==res]:
+            
+            if verbose: print('Building band %s at %s m resolution'%(band, str(res)))
+            
+            band_out, QA_out = sen2mosaic.mosaic.buildMosaic(scenes_reduced, band, md_dest, output_dir = output_dir, output_name = output_name, colour_balance = colour_balance, improve_mask = improve_mask, percentile = 25., processes = processes, step = 2000, masked_vals = masked_vals, output_mask = output_mask, temp_dir = temp_dir, verbose = verbose)            
+            
+            # Only output mask on first iteration
+            output_mask = False
+            
+        # Build VRT output files for straightforward visualisation
+        if verbose: print('Building .VRT images for visualisation')
+        
+        # Natural colour image (10 m)
+        sen2mosaic.mosaic.buildVRT('%s/%s_R%sm_B04.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_B03.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_B02.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_RGB.vrt'%(output_dir, output_name, str(res)))
+
+        # Near infrared image. Band at (10 m) has a different format to bands at 20 and 60 m.
+        if res == 10:
+            sen2mosaic.mosaic.buildVRT('%s/%s_R%sm_B08.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_B04.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_B03.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_NIR.vrt'%(output_dir, output_name, str(res)))    
+        else:
+            sen2mosaic.mosaic.buildVRT('%s/%s_R%sm_B8A.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_B04.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_B03.tif'%(output_dir, output_name, str(res)), '%s/%s_R%sm_NIR.vrt'%(output_dir, output_name, str(res)))
+        
+    print('Processing complete!')
+
+
+if __name__ == "__main__":
+    
+    # Set up command line parser    
+
+    parser = argparse.ArgumentParser(description = "Process Sentinel-2 data to a composite mosaic product to a customisable grid square, based on specified UTM coordinate bounds. Data are output as GeoTiff files for each spectral band, with .vrt files for ease of visualisation.")
+
+    parser._action_groups.pop()
+    positional = parser.add_argument_group('positional arguments')
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+    
+    # Positional arguments
+    positional.add_argument('infiles', metavar = 'PATH', type = str, default = [os.getcwd()], nargs = '*', help = 'Sentinel 2 input files (level 1C/2A) in .SAFE format. Specify one or more valid Sentinel-2 .SAFE, a directory containing .SAFE files, a Sentinel-2 tile or multiple granules through wildcards (e.g. *.SAFE/GRANULE/*), or a file containing a list of input files. Leave blank to process files in current working directoy. All granules that match input conditions will be included.')
+    
+    # Required arguments
+    required.add_argument('-te', '--target_extent', nargs = 4, metavar = ('XMIN', 'YMIN', 'XMAX', 'YMAX'), type = float, required = True, help = "Extent of output image tile, in format <xmin, ymin, xmax, ymax>.")
+    required.add_argument('-e', '--epsg', metavar = 'EPSG', type=int, required = True, help="EPSG code for output image tile CRS. This must be UTM. Find the EPSG code of your output CRS as https://www.epsg-registry.org/.")
+    
+    # Optional arguments
+    optional.add_argument('-l', '--level', type=str, metavar='1C/2A', default = '2A', help = "Input image processing level, '1C' or '2A'. Defaults to '2A'.")
+    optional.add_argument('-st', '--start', type = str, default = '20150101', help = "Start date for tiles to include in format YYYYMMDD. Defaults to processing all dates.")
+    optional.add_argument('-en', '--end', type = str, default = datetime.datetime.today().strftime('%Y%m%d'), help = "End date for tiles to include in format YYYYMMDD. Defaults to processing all dates.")
+    optional.add_argument('-res', '--resolution', metavar = '10/20/60', type=int, default = 0, help="Specify a resolution to process (10, 20, 60, or 0 for all).")
+    optional.add_argument('-m', '--masked_vals', metavar = 'N', type=str, nargs='*', default = ['auto'], help="Specify SLC values to not include in the mosaic (e.g. -m 7 8 9). See http://step.esa.int/main/third-party-plugins-2/sen2cor/ for description of sen2cor mask values. Defaults to 'auto', which masks values 0 and 9. Also accepts 'none', to include all values.")
+    optional.add_argument('-b', '--colour_balance', action='store_true', default = False, help = "Perform colour balancing between tiles. Defaults to False. Not generally recommended, particularly where working over large areas.")
+    optional.add_argument('-i', '--improve_mask', action='store_true', default = False, help = "Apply improvements to Sentinel-2 cloud mask. Not generally recommended, except where a very conservative mask is desired. Defaults to no improvement.")
+    optional.add_argument('-t', '--temp_dir', type=str, metavar = 'DIR', default = '/tmp', help="Directory to write temporary files, only required for L1C data. Defaults to '/tmp'.")
+    optional.add_argument('-o', '--output_dir', type=str, metavar = 'DIR', default = os.getcwd(), help="Specify an output directory. Defaults to the present working directory.")
+    optional.add_argument('-n', '--output_name', type=str, metavar = 'NAME', default = 'mosaic', help="Specify a string to precede output filename. Defaults to 'mosaic'.")
+    optional.add_argument('-p', '--n_processes', type = int, metavar = 'N', default = 1, help = "Specify a maximum number of tiles to process in paralell. Bear in mind that more processes will require more memory. Defaults to 1.")
+    optional.add_argument('-v', '--verbose', action='store_true', default = False, help = "Make script verbose.")
+
+    # Get arguments
+    args = parser.parse_args()
+        
+    assert args.level in ['1C', '2A'], "Input level much be '1C' or '2A'."
+    
+    # Convert masked_vals to integers, where specified
+    if args.masked_vals != ['auto'] and args.masked_vals != ['none']:
+        masked_vals = [int(m) for m in args.masked_vals]
+    else:
+        masked_vals = args.masked_vals[0]
+    
+    # Get absolute path of input .safe files.
+    infiles = sorted([os.path.abspath(i) for i in args.infiles])
+    
+    # Find all matching granule files
+    infiles = sen2mosaic.utilities.prepInfiles(infiles, args.level)
+    
+    main(infiles, args.target_extent, args.epsg, resolution = args.resolution, start = args.start, end = args.end, improve_mask = args.improve_mask, colour_balance = args.colour_balance, processes = args.n_processes, output_dir = args.output_dir, output_name = args.output_name, masked_vals = masked_vals, temp_dir = args.temp_dir, verbose = args.verbose)
+    
+    
---
+++ sen2mosaic/IO.py
@@ -0,0 +1,238 @@
+
+
+import datetime
+import glob
+import numpy as np
+from osgeo import gdal, osr
+
+import xml.etree.ElementTree as ET
+
+
+### Functions for data input and output, and image reprojection
+
+
+#########################################
+### Geospatial manipulation functions ###
+#########################################
+
+def reprojectBand(scene, data, md_dest, dtype = 2, resampling = 0):
+    """
+    Funciton to load, correct and reproject a Sentinel-2 array
+    
+    Args:
+        scene: A level-2A scene of class utilities.LoadScene().
+        data: The array to reproject
+        md_dest: An object of class utilities.Metadata() to reproject image to.
+    
+    Returns:
+        A numpy array of resampled mask data
+    """
+    
+    # Write mask array to a gdal dataset
+    ds_source = createGdalDataset(scene.metadata, data_out = data, dtype = dtype)
+        
+    # Create an empty gdal dataset for destination
+    ds_dest = createGdalDataset(md_dest, dtype = dtype)
+    
+    # Reproject source to destination projection and extent
+    data_resampled = reprojectImage(ds_source, ds_dest, scene.metadata, md_dest, resampling = resampling)
+    
+    return data_resampled
+
+
+def createGdalDataset(md, data_out = None, filename = '', driver = 'MEM', dtype = 3, RasterCount = 1, nodata = None, options = []):
+    '''
+    Function to create an empty gdal dataset with georefence info from metadata dictionary.
+
+    Args:
+        md: Object from Metadata() class.
+        data_out: Optionally specify an array of data to include in the gdal dataset.
+        filename: Optionally specify an output filename, if image will be written to disk.
+        driver: GDAL driver type (e.g. 'MEM', 'GTiff'). By default this function creates an array in memory, but set driver = 'GTiff' to make a GeoTiff. If writing a file to disk, the argument filename must be specified.
+        dtype: Output data type. Default data type is a 16-bit unsigned integer (gdal.GDT_Int16, 3), but this can be specified using GDAL standards.
+        options: A list containing other GDAL options (e.g. for compression, use [compress='LZW'].
+
+    Returns:
+        A GDAL dataset.
+    '''
+    from osgeo import gdal, osr
+        
+    gdal_driver = gdal.GetDriverByName(driver)
+    ds = gdal_driver.Create(filename, md.ncols, md.nrows, RasterCount, dtype, options = options)
+    
+    ds.SetGeoTransform(md.geo_t)
+    
+    proj = osr.SpatialReference()
+    proj.ImportFromEPSG(md.EPSG_code)
+    ds.SetProjection(proj.ExportToWkt())
+    
+    # If a data array specified, add data to the gdal dataset
+    if type(data_out).__module__ == np.__name__:
+        
+        if len(data_out.shape) == 2:
+            data_out = np.ma.expand_dims(data_out,2)
+        
+        for feature in range(RasterCount):
+            ds.GetRasterBand(feature + 1).WriteArray(data_out[:,:,feature])
+            
+            if nodata != None:
+                ds.GetRasterBand(feature + 1).SetNoDataValue(nodata)
+    
+    # If a filename is specified, write the array to disk.
+    if filename != '':
+        ds = None
+    
+    return ds
+
+
+def reprojectImage(ds_source, ds_dest, md_source, md_dest, resampling = 0):
+    '''
+    Reprojects a source image to match the coordinates of a destination GDAL dataset.
+    
+    Args:
+        ds_source: A gdal dataset from utilities.createGdalDataset() containing data to be repojected.
+        ds_dest: A gdal dataset from utilities.createGdalDataset(), with destination coordinate reference system and extent.
+        md_source: Metadata class from utilities.Metadata() representing the source image.
+        md_dest: Metadata class from utilities.Metadata() representing the destination image.
+    
+    Returns:
+        A GDAL array with resampled data
+    '''
+    
+    from osgeo import gdal
+    
+    def _copyds(ds):
+        '''
+        Build a copy of an input ds, where performing fix on nodata values
+        '''
+        
+        proj = osr.SpatialReference(wkt=ds.GetProjection())
+        proj.AutoIdentifyEPSG()
+        epsg = int(proj.GetAttrValue('AUTHORITY',1))
+                
+        geo_t = ds.GetGeoTransform()
+        ulx = geo_t[0]
+        lrx = geo_t[0] + (geo_t[1] * ds.RasterXSize)
+        lry = geo_t[3] + (geo_t[5] * ds.RasterYSize)
+        uly = geo_t[3]
+        
+        extent = [ulx, lry, lrx, uly]
+                
+        md = Metadata(extent, ds.GetGeoTransform()[1], epsg)
+        return createGdalDataset(md, dtype = 1)
+    
+    proj_source = md_source.proj.ExportToWkt()
+    proj_dest = md_dest.proj.ExportToWkt()
+    
+    # Reproject source into dest project coordinates
+    gdal.ReprojectImage(ds_source, ds_dest, proj_source, proj_dest, resampling)
+            
+    ds_resampled = ds_dest.GetRasterBand(1).ReadAsArray()
+    
+    # As GDAL fills in all nodata pixels as zero, re-do transfromation with array of ones and re-allocate zeros to nodata. Only run where a nodata value has been assigned to ds_source.
+    if ds_source.GetRasterBand(1).GetNoDataValue() is not None:
+        
+        ds_source_mask = _copyds(ds_source)
+        ds_dest_mask = _copyds(ds_dest)
+        ds_source_mask.GetRasterBand(1).WriteArray(np.ones_like(ds_source.GetRasterBand(1).ReadAsArray()))
+        gdal.ReprojectImage(ds_source_mask, ds_dest_mask, proj_source, proj_dest, gdal.GRA_NearestNeighbour)
+        ds_resampled[ds_dest_mask.GetRasterBand(1).ReadAsArray() == 0] = ds_source.GetRasterBand(1).GetNoDataValue()
+    
+    return ds_resampled
+
+
+
+###########################
+### Sentinel-2 metadata ###
+###########################
+
+
+def getMetadata(granule_file, resolution = 20, level = '2A', tile = ''):
+    '''
+    Function to extract georefence info from level 1C/2A Sentinel 2 data in .SAFE format.
+    
+    Args:
+        granule_file: String with /path/to/the/granule folder bundled in a .SAFE file.
+        resolution: Integer describing pixel size in m (10, 20, or 60). Defaults to 20 m.
+
+    Returns:
+        A list describing the extent of the .SAFE file granule, in the format [xmin, ymin, xmax, ymax].
+        EPSG code of the coordinate reference system of the granule
+    '''
+    
+    assert resolution in [10, 20, 60], "Resolution must be 10, 20 or 60 m."
+    assert level in ['1C', '2A'], "Product level must be either '1C' or '2A'."
+    
+    # Remove trailing / from granule directory if present 
+    granule_file = granule_file.rstrip('/')
+    
+    assert len(glob.glob((granule_file + '/*MTD*.xml'))) > 0, "The location %s does not contain a metadata (*MTD*.xml) file."%granule_file
+    
+    # Find the xml file that contains file metadata
+    xml_file = glob.glob(granule_file + '/*MTD*.xml')[0]
+    
+    # Parse xml file
+    tree = ET.ElementTree(file = xml_file)
+    root = tree.getroot()
+            
+    # Define xml namespace
+    ns = {'n1':root.tag[1:].split('}')[0]}
+    
+    # Get array size
+    size = root.find("n1:Geometric_Info/Tile_Geocoding[@metadataLevel='Brief']/Size[@resolution='%s']"%str(resolution),ns)
+    nrows = int(size.find('NROWS').text)
+    ncols = int(size.find('NCOLS').text)
+    
+    # Get extent data
+    geopos = root.find("n1:Geometric_Info/Tile_Geocoding[@metadataLevel='Brief']/Geoposition[@resolution='%s']"%str(resolution),ns)
+    ulx = float(geopos.find('ULX').text)
+    uly = float(geopos.find('ULY').text)
+    xres = float(geopos.find('XDIM').text)
+    yres = float(geopos.find('YDIM').text)
+    lrx = ulx + (xres * ncols)
+    lry = uly + (yres * nrows)
+    
+    extent = [ulx, lry, lrx, uly]
+    
+    # Find EPSG code to define projection
+    EPSG = root.find("n1:Geometric_Info/Tile_Geocoding[@metadataLevel='Brief']/HORIZONTAL_CS_CODE",ns).text
+    EPSG = int(EPSG.split(':')[1])
+    
+    # Get datetime
+    datestring = root.find("n1:General_Info/SENSING_TIME[@metadataLevel='Standard']",ns).text.split('.')[0]
+    date = datetime.datetime.strptime(datestring,'%Y-%m-%dT%H:%M:%S')
+    
+    if level == '2A':
+        try:
+            # Get nodata percentage based on scene classification
+            vegetated = root.find("n1:Quality_Indicators_Info[@metadataLevel='Standard']/L2A_Image_Content_QI/VEGETATION_PERCENTAGE",ns).text
+            not_vegetated = root.find("n1:Quality_Indicators_Info[@metadataLevel='Standard']/L2A_Image_Content_QI/NOT_VEGETATED_PERCENTAGE",ns).text
+            water = root.find("n1:Quality_Indicators_Info[@metadataLevel='Standard']/L2A_Image_Content_QI/WATER_PERCENTAGE",ns).text
+        except:
+            # In case of new sen2cor format
+            vegetated = root.find("n1:Quality_Indicators_Info[@metadataLevel='Standard']/Image_Content_QI/VEGETATION_PERCENTAGE",ns).text
+            not_vegetated = root.find("n1:Quality_Indicators_Info[@metadataLevel='Standard']/Image_Content_QI/NOT_VEGETATED_PERCENTAGE",ns).text
+            water = root.find("n1:Quality_Indicators_Info[@metadataLevel='Standard']/Image_Content_QI/WATER_PERCENTAGE",ns).text
+            
+        nodata_percent = 100. - float(water) - float(vegetated) - float(not_vegetated)
+    
+    elif level == '1C':
+        # Get nodata percentrage based on estimated cloud cover
+        cloud_cover = root.find("n1:Quality_Indicators_Info[@metadataLevel='Standard']/Image_Content_QI/CLOUDY_PIXEL_PERCENTAGE", ns).text
+        
+        nodata_percent = 100. - float(cloud_cover)
+    
+    if tile == '':
+        # Get tile from granule filename
+        if granule_file.split('/')[-1].split('_')[1] == 'USER':
+            
+            # If old file format
+            tile = granule_file.split('/')[-1].split('_')[-2]
+            
+        else:
+            
+            # If new file format
+            tile = granule_file.split('/')[-1].split('_')[1]
+    
+    return extent, EPSG, date, tile, nodata_percent
+
